6abc9f8f46137129f268764bde9ab9b49aaa1028
Berkay Kaptan <kaptanberkay35@hotmail.com>
2024-01-29 14:10:09 +0300

    Configurations-completed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="bg-slate-950 h-screen flex justify-center items-center">
+        <p className="text-blue-600">deneme</p>
+      </div>
+    );
+  }
+  
