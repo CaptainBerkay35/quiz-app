d089aa9bce27e31d4bf113f1cf2b8537e463d3c9
Berkay Kaptan <kaptanberkay35@hotmail.com>
2024-01-30 11:26:28 +0300

    Home page completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+import logo from "../logo.svg";
+
 export default function Home() {
-    return (
-      <div className="bg-slate-950 h-screen flex justify-center items-center">
-        <p className="text-blue-600">deneme</p>
-      </div>
-    );
+  const [username, setUsername] = useState();
+
+  function saveUsername(event) {
+    setUsername(event.target.value);
   }
-  
+
+  return (
+    <div className="bg-gradient-to-t from-blue-500 to-blue-950 h-screen flex flex-col justify-center items-center ">
+      <img className="mb-4 w-20" src={logo} alt="Logo" />
+      <h1 className="text-white font-bold text-3xl mb-4">Quizo</h1>
+      <p className="text-white w-72 text-center leading-loose mb-4">
+        Are you ready to test your knowledge?Before you move on to challange let
+        us know your name
+      </p>
+      <input
+        type="text"
+        className="border-2 border-blue-900 rounded mb-4 h-8"
+        value={username} 
+        onChange={saveUsername} 
+      />
+      <button className="bg-white px-12 py-3 rounded-lg text-blue-600 font-bold border-2 border-blue-900 shadow-xl ">
+        Start
+      </button>
+    </div>
+  );
+}
